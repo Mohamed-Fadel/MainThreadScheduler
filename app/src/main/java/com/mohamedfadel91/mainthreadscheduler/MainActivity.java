ec08d3efc8883b9cbf1d3e49cd4c6ae2c9eea5e0
Fadel <mohamedfadel91@gmail.com>
2018-03-17 21:46:11 +0200

    sample to get width of editText as soon as mainThread become idle

--- app/src/main/java/com/mohamedfadel91/mainthreadscheduler/MainActivity.java
+++ app/src/main/java/com/mohamedfadel91/mainthreadscheduler/MainActivity.java
@@ -1,13 +1,31 @@
 package com.mohamedfadel91.mainthreadscheduler;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mohamedfadel91.mainthreadschedulerlib.MainThreadScheduler;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edt_email);
+
+        MainThreadScheduler.scheduleWhenIdle(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this,
+                        " " + edtEmail.getWidth(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
